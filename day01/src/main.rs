05ee08386480ff10082903535bdf7ed83f2818f9
Luke Taylor <luke@bigring.org>
2025-12-03 00:41:19 -0500

    Day 1 - Simplify

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
         .trim()
         .split_whitespace()
         .map(|instr| {
-            let multiplier = if instr.chars().nth(0).expect("bruh") == 'L' {
+            let multiplier = if instr.chars().nth(0).unwrap() == 'L' {
                 -1
             } else {
                 1
@@ -28,11 +28,15 @@ fn main() {
     println!("{}", part_2(&nums));
 }
 
+fn wrap_num(num: i32, wrap_bound: i32) -> i32 {
+    ((num % wrap_bound) + wrap_bound) % wrap_bound
+}
+
 fn part_1(nums: &Vec<i32>) -> i32 {
     let mut current_val = DIAL_START;
     let mut zero_count = 0;
     nums.iter().for_each(|instr| {
-        current_val = neg_inclusive_modulo(current_val + instr, DIAL_SIZE);
+        current_val = wrap_num(current_val + instr, DIAL_SIZE);
         if current_val == 0 {
             zero_count += 1
         }
@@ -40,27 +44,22 @@ fn part_1(nums: &Vec<i32>) -> i32 {
     return zero_count;
 }
 
-fn neg_inclusive_modulo(num: i32, mod_factor: i32) -> i32 {
-    ((num % mod_factor) + mod_factor) % mod_factor
-}
-
 fn part_2(nums: &Vec<i32>) -> i32 {
     let mut current_val = DIAL_START;
     let mut zero_count = 0;
+
     nums.iter().for_each(|instr| {
         let full_rot_crosses = (instr / DIAL_SIZE).abs();
         zero_count += full_rot_crosses;
 
-        let result = neg_inclusive_modulo(current_val + instr, DIAL_SIZE);
-        if result == 0 {
+        let raw_rotation = current_val + (instr % DIAL_SIZE);
+        let wrapped_rotation = wrap_num(current_val + instr, DIAL_SIZE);
+        if (wrapped_rotation != raw_rotation && current_val != 0) || wrapped_rotation == 0 {
             zero_count += 1;
-        } else if current_val != 0 && (*instr < 0) && result > current_val {
-            zero_count += 1;
-        } else if (*instr > 0) && result < current_val {
-            zero_count += 1;
-        };
+        }
 
-        current_val = result;
+        current_val = wrapped_rotation;
     });
-    return zero_count;
+
+    zero_count
 }
